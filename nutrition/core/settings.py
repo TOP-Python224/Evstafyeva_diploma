462fa7b4c391ce4f8c6073c59c7c55dad79f2a13
Gennadiy <renardes@gmail.com>
2023-04-17 16:38:38 +0500

    ошибка при подключении django приложения

--- nutrition/core/settings.py
+++ nutrition/core/settings.py
@@ -33,13 +33,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # встроенные приложения
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.Users'
+    # собственные приложения
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
